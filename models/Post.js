79cbf93dbad39be9f9f79df13cb1e37c5048e61e
jennifergoldberg <jagoldbe@gmail.com>
2021-08-01 12:02:47 -0400

    added drawio for ERD

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,5 @@
 const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  avatar
+})
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 
-// Module instance
+// Module Instance
 const app = express();
 
 // PORT
