80d8434e25052f94024f114b20e93a553613cf1f
gtiamzon <gerald.tiamzon@yahoo.com>
2021-08-01 15:22:21 -0700

    new route and create route made

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -3,11 +3,20 @@ const router = express.Router();
 
 const { Post } = require("../models");
 
-// index router
+// index route - GET
 router.get('/', (req,res) => {
   res.send("I am the index route");
 });
 
+// new route - presentational - GET
+router.get('/posts/new', (req, res) => {
+  res.send("I am the new route");
+});
+
+// create - functional - POST
+router.post('/', (req, res)=> {
+  res.render('new.ejs');
+});
 
 
 module.exports = router;
