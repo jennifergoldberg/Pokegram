60382d46eb60c7eac34866e127c44dd08749299a
jennifergoldberg <jagoldbe@gmail.com>
2021-08-04 18:01:29 -0400

    working on likes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,19 @@
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
+// var myModal = document.getElementById('myModal')
+// var myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus()
+// });
+
+// const likesCount = 0;
+// const postLikes = document.getElementById('#post-likes')
+// postLikes.onclick("click", )
+
+// $("#post-likes src").on("click", () => {
+//   $("#post-likes src").attr("src", "/assets/_Favourite.png")
+// });
+
+
+document.querySelector(".post-likes").addEventListener("click", () => {
+  (".post-likes").setAttribute("src", "/assets/_Favourite.png")
 });
-
-const likesCount = 0;
-
-$("#post-likes src").on("click", () => {
-  likesCount+1
-  $("#post-likes src").appendTo("src", "/assets/_Favourite.png")
-});
-
-
