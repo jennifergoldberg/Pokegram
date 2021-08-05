ad47041e37b06af450eccc79ebcd0dd7eabac93b
jennifergoldberg <jagoldbe@gmail.com>
2021-08-05 16:47:40 -0400

    working on deploying

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 require("../config/db.connection");
 
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/pokegram');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pokegram');
 
 module.exports = {
   Post: require("./Post"),
